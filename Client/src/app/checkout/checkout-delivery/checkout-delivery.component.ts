edcb4bc33c7acd3c917842c06a18e2ff25704c40
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2023-05-28 08:06:07 +0530

    Error in delivery component

--- Client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ Client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -8,16 +8,16 @@ import { CheckoutService } from '../checkout.service';
   templateUrl: './checkout-delivery.component.html',
   styleUrls: ['./checkout-delivery.component.scss']
 })
-export class CheckoutDeliveryComponent {
-  // @Input() checkoutForm?: FormGroup;
-  // deliveryMethods: DeliveryMethod[] = [];
+export class CheckoutDeliveryComponent implements OnInit {
+  @Input() checkoutForm?: FormGroup;
+  deliveryMethods: DeliveryMethod[] = [];
 
-  // constructor(private checkoutService: CheckoutService) {}
+  constructor(private checkoutService: CheckoutService) {}
 
-  // ngOnInit(): void {
-  //   this.checkoutService.getDeliveryMethods().subscribe({
-  //     next: dm => this.deliveryMethods = dm
-  //   })
-  // }
+  ngOnInit(): void {
+    this.checkoutService.getDeliveryMethods().subscribe({
+      next: dm => this.deliveryMethods = dm
+    })
+  }
   
 }
